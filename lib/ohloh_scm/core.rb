6898a69e024c505474eaeea94edfb70789e9b65d
Priya Rani <priyar@blackduck.com>
2025-05-27 14:38:21 +0530

    Fix keyword arguments

--- lib/ohloh_scm/core.rb
+++ lib/ohloh_scm/core.rb
@@ -9,11 +9,13 @@ module OhlohScm
     attr_reader :scm, :activity, :status, :validation
 
     def initialize(scm_type, url, branch_name, username, password)
+     
       scm_opts = { core: self, url: url, branch_name: branch_name,
                    username: username, password: password }
+
       scm_class_name = scm_type.to_s.camelize
 
-      @scm = OhlohScm.const_get(scm_class_name)::Scm.new(scm_opts)
+      @scm = OhlohScm.const_get(scm_class_name)::Scm.new(**scm_opts)
       @activity = OhlohScm.const_get(scm_class_name)::Activity.new(self)
       @status = OhlohScm.const_get(scm_class_name)::Status.new(self)
       @validation = OhlohScm.const_get(scm_class_name)::Validation.new(self)
